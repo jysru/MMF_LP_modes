85be7cef1d5011115b8b7654c210895b44635d42
JSaucourt <jysru@pm.me>
2023-03-06 10:34:46 +0100

    Added offset

--- CameraGrid.m
+++ CameraGrid.m
@@ -4,6 +4,7 @@ classdef CameraGrid < handle
     properties (SetAccess = private, GetAccess = public)
         pixel_size
         pixel_numbers
+        offsets
         x
         y
         X
@@ -23,13 +24,15 @@ classdef CameraGrid < handle
             arguments
                 opts.pixel_size (1,1) double {mustBeNumeric, mustBePositive} = 5e-6
                 opts.pixel_numbers (1,2) double {mustBeNumeric, mustBePositive} = [128, 128]
+                opts.offsets (1,2) double {mustBeNumeric} = [0, 0]
             end
 
             obj.pixel_size = opts.pixel_size;
             obj.pixel_numbers = opts.pixel_numbers;
+            obj.offsets = opts.offsets;
             
-            obj.x = [ -obj.grid_sizes(1)/2 : obj.pixel_size : obj.grid_sizes(1)/2-obj.pixel_size ];
-            obj.y = [ -obj.grid_sizes(2)/2 : obj.pixel_size : obj.grid_sizes(2)/2-obj.pixel_size ];
+            obj.x = (-obj.grid_sizes(1)/2 : obj.pixel_size : obj.grid_sizes(1)/2-obj.pixel_size) - obj.offsets(1);
+            obj.y = (-obj.grid_sizes(2)/2 : obj.pixel_size : obj.grid_sizes(2)/2-obj.pixel_size) - obj.offsets(2);
 
             [obj.X, obj.Y] = meshgrid(obj.x, obj.y);
         end
