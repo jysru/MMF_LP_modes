352ba7cb55cf05525a3bb31418ac8f0f0a154a17
JSaucourt <jysru@pm.me>
2023-03-06 10:49:10 +0100

    Update main.m

--- main.m
+++ main.m
@@ -1,12 +1,10 @@
 clear all
 
-consts = modes_grin_betas();
-[intens, X, Y] = modal_sum(consts, n_modes=10, noise=0.02);
+grid = CameraGrid(pixel_numbers=[256, 256], pixel_size=0.25e-6, offsets=[0,0]*1e-6);
+fiber = GrinFiber();
+speckle = GrinSpeckle(fiber, grid, noise=0.02, N_modes=4);
+speckle.plot()
 
-figure(1), clf
-imagesc(X(1,:)*1e6, Y(:,1)*1e6, intens)
-axis square
-colorbar
-colormap('hot')
-xlabel('x [um]')
-ylabel('y [um]')
+
+dset = GrinLPDataset(fiber, grid);
+
