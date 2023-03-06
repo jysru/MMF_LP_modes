1414ce85cb1ab199bb8dc9b90a191ebf41460325
JSaucourt <jysru@pm.me>
2023-03-06 10:28:50 +0100

    Fixed grid sizes

--- CameraGrid.m
+++ CameraGrid.m
@@ -1,7 +1,7 @@
 classdef CameraGrid < handle
     % CameraGrid defines grids to handle computations
 
-    properties (SetAccess = private, GetAccess = public, Hidden)
+    properties (SetAccess = private, GetAccess = public)
         pixel_size
         pixel_numbers
         x
@@ -10,11 +10,13 @@ classdef CameraGrid < handle
         Y
     end
 
+
     properties (Dependent)
         grid_sizes
         R
         A
     end
+
 
     methods
         function obj = CameraGrid(opts)
@@ -26,19 +28,22 @@ classdef CameraGrid < handle
             obj.pixel_size = opts.pixel_size;
             obj.pixel_numbers = opts.pixel_numbers;
             
-            obj.x = [ -obj.grid_sizes(1)/2 : obj.pixel_size : obj.grid_sizes(1)/2 ];
-            obj.y = [ -obj.grid_sizes(2)/2 : obj.pixel_size : obj.grid_sizes(2)/2 ];
+            obj.x = [ -obj.grid_sizes(1)/2 : obj.pixel_size : obj.grid_sizes(1)/2-obj.pixel_size ];
+            obj.y = [ -obj.grid_sizes(2)/2 : obj.pixel_size : obj.grid_sizes(2)/2-obj.pixel_size ];
 
             [obj.X, obj.Y] = meshgrid(obj.x, obj.y);
         end
+
 
         function val = get.grid_sizes(obj)
             val = obj.pixel_size * obj.pixel_numbers;
         end
 
+
         function val = get.R(obj)
             val = sqrt( obj.X.^2 + obj.Y.^2);
         end
+
 
         function val = get.A(obj)
             val = atan2(obj.Y, obj.X);
