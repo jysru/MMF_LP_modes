d372e63b0a7eb2164da8ab4ce011549e038e3f93
JSaucourt <jysru@pm.me>
2023-03-15 13:37:09 +0100

    Update example.m

--- example.m
+++ example.m
@@ -7,15 +7,23 @@ fiber = GrinFiber();
 
 
 %% Check LP mode polarizations on grid and fiber
-mode = GrinLPMode(2,2);
+mode = GrinLPMode(3,1);
 mode.compute(fiber, grid);
 mode.plot(fig_num=1);
 
 
 %% Generate speckle from first modes with some noise
-speckle = GrinSpeckle(fiber, grid, noise=0.02, N_modes=25);
-speckle.plot(fig_num=2)
+speckle = GrinSpeckle(fiber, grid, noise=0.00, N_modes=25);
+speckle.plot(fig_num=2);
 
+
+%% Modal decomposition
+modals = LPModalDecomposition(speckle, N_modes=25);
+disp([abs(modals.modes_coeffs), abs(speckle.modes_coeffs)])
 
 %% Generate dataset of individual LP modes
-dset = GrinLPDataset(fiber, grid, N_modes=opts.N_modes, noise=opts.noise);
+% dset = GrinLPDataset(fiber, grid, N_modes=opts.N_modes, noise=opts.noise);
+
+
+%% Generate dataset of LP modes sums
+% dset = GrinLPSpeckleDataset(fiber, grid, "N_modes",10, "length",10, "noise",0.01);
