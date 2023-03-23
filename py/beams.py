d523f4ad37691c0b80cc899b353a4f93510a7337
JSaucourt <jysru@pm.me>
2023-03-23 09:40:05 +0100

    Refactored including general grid and added plot method

--- py/beams.py
+++ py/beams.py
@@ -1,57 +1,58 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
 import scipy.special as sp
 
-
-class Grid():
-
-    def __init__(self, size: int=128) -> None:
-        self.size = np.abs(size)
-        self.x_vec = np.linspace(start=0, stop=self.size, num=self.size)
-        self.y_vec = np.linspace(start=0, stop=self.size, num=self.size)
-        self.x_grid, self.y_grid = np.meshgrid(self.x_vec, self.y_vec)
-
-    @property
-    def centers(self):
-        return np.array([self.size//2, self.size//2])
-
+from grid import Grid
         
 
 class Beam():
 
     def __init__(self, grid: Grid) -> None:
         self.grid = grid
-        self.field = np.zeros_like(self.grid.x_grid, dtype=float)
+        self.field = np.zeros(shape=(grid.pixel_numbers), dtype=float)
 
     def _add_offsets(self, offsets: list[int]= None) -> list[int]:
         if offsets:
-            centers = self.grid.centers + np.array(offsets)
+            centers = self.grid.offsets + np.array(offsets)
         else:
-            centers = self.grid.centers
+            centers = self.grid.offsets
         return centers
 
     def gen_gaussian(self, amplitude: float=1, width: float=10, centers: list[int]=None):
         centers = self._add_offsets(centers)
-        self.field = amplitude*np.exp(-((np.power(self.grid.x_grid-centers[0],2) + np.power(self.grid.y_grid-centers[1],2))/np.power(width,2)))
+        self.field = amplitude * np.exp(
+            -((np.square(self.grid.X - centers[0]) + np.square(self.grid.Y - centers[1]))
+            / (2 * np.square(width)))
+            )
 
     def gen_bessel(self, amplitude: float=1, order=1, width: float=10, centers: list[int]=None):
         centers = self._add_offsets(centers)
-        arg = np.sqrt(np.power(self.grid.x_grid - centers[0],2) + np.power(self.grid.y_grid - centers[1],2))
-        self.field = sp.jn(order, arg/width)
-        self.field = amplitude*self.field/np.max(self.field)
+        arg = np.sqrt(np.power(self.grid.X - centers[0], 2) + np.power(self.grid.Y - centers[1], 2))
+        self.field = sp.jn(order, arg / width)
+        self.field = amplitude * self.field / np.max(self.field)
 
     @property
     def intensity(self):
         return np.power(self.field,2)
-
+    
+    def plot(self, cmap: str = 'hot', extent_coeff: float = 1e6):
+        extent = np.array([np.min(self.grid.x), np.max(self.grid.x), np.min(self.grid.y), np.max(self.grid.y)]) * extent_coeff
+        fig = plt.figure()
+        ax = plt.gca()
+        pl = plt.imshow(self.intensity, cmap=cmap, extent=extent)
+        ax.set_xlabel("x [um]")
+        ax.set_ylabel("y [um]")
+        ax.set_title(f"Beam intensity")
+        plt.colorbar(pl, ax=ax)
+        return (fig, ax, pl)
 
 
 if __name__ == "__main__":
-    grid = Grid(122)
+    grid = Grid(pixel_size=2e-6)
     beam = Beam(grid)
-    beam.gen_bessel(order=1, amplitude=1, width=10, centers=[0,0])
+    # print(beam.field.shape)
+    # beam.gen_bessel(order=1, amplitude=1, width=10e-6)
+    beam.gen_bessel(order=2, amplitude=1, width=20e-6, centers=[0,0])
 
-    fig = plt.figure()
-    sns.heatmap(beam.intensity, square=True)
+    beam.plot()
     plt.show()
