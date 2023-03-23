de022728ddc4b8c7bfd3fd44550f06f8ead28703
JSaucourt <jysru@pm.me>
2023-03-23 10:03:19 +0100

    Beam class is now abstract

--- py/beams.py
+++ py/beams.py
@@ -1,35 +1,25 @@
+from abc import ABC, abstractmethod
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.special as sp
 
 from grid import Grid
-        
 
-class Beam():
+
+class Beam(ABC):
 
     def __init__(self, grid: Grid) -> None:
         self.grid = grid
         self.field = np.zeros(shape=(grid.pixel_numbers), dtype=float)
+        self.centers = None
+        self.amplitude = None
 
-    def _add_offsets(self, offsets: list[int]= None) -> list[int]:
-        if offsets:
-            centers = self.grid.offsets + np.array(offsets)
-        else:
-            centers = self.grid.offsets
-        return centers
+    def _add_offsets(self, offsets: list[int]= None) -> None:
+        self.centers = self.grid.offsets + np.array(offsets) if offsets else self.grid.offsets
 
-    def gen_gaussian(self, amplitude: float=1, width: float=10, centers: list[int]=None):
-        centers = self._add_offsets(centers)
-        self.field = amplitude * np.exp(
-            -((np.square(self.grid.X - centers[0]) + np.square(self.grid.Y - centers[1]))
-            / (2 * np.square(width)))
-            )
-
-    def gen_bessel(self, amplitude: float=1, order=1, width: float=10, centers: list[int]=None):
-        centers = self._add_offsets(centers)
-        arg = np.sqrt(np.power(self.grid.X - centers[0], 2) + np.power(self.grid.Y - centers[1], 2))
-        self.field = sp.jn(order, arg / width)
-        self.field = amplitude * self.field / np.max(self.field)
+    @abstractmethod
+    def compute(self):
+        return
 
     @property
     def intensity(self):
@@ -49,14 +39,69 @@ class Beam():
         ax.set_title(f"Beam intensity")
         plt.colorbar(pl, ax=ax)
         return (fig, ax, pl)
+    
+
+class GaussianBeam(Beam):
+
+    def __init__(self, grid: Grid) -> None:
+        super().__init__(grid)
+        self.width = None
+
+    def compute(self, amplitude: float = 1, width: float = 10e-6, centers: list[int] = None):
+        self._add_offsets(centers)
+        self.amplitude = amplitude
+        self.width = width
+
+        self.field = amplitude * np.exp(
+            -((np.square(self.grid.X - self.centers[0]) + np.square(self.grid.Y - self.centers[1]))
+            / (2 * np.square(width)))
+            )
+        
+    def __str__(self) -> str:
+        return (
+            f"{__class__.__name__} instance with:\n"
+            f"  - Amplitude: {self.amplitude}\n"
+            f"  - Width: {self.width}\n"
+            f"  - Centers: {self.centers}\n"
+        )
+        
+
+class BesselBeam(Beam):
+
+    def __init__(self, grid: Grid) -> None:
+        super().__init__(grid)
+        self.width = None
+        self.order = None
+
+    def compute(self, amplitude: float = 1, order: int = 1, width: float = 10e-6, centers: list[int] = None):
+        self._add_offsets(centers)
+        self.amplitude = amplitude
+        self.width = width
+        self.order = order
+
+        arg = np.sqrt(np.power(self.grid.X - self.centers[0], 2) + np.power(self.grid.Y - self.centers[1], 2))
+        self.field = sp.jn(order, arg / width)
+        self.field = amplitude * self.field / np.max(self.field)
+
+    def __str__(self) -> str:
+        return (
+            f"{__class__.__name__} instance with:\n"
+            f"  - Amplitude: {self.amplitude}\n"
+            f"  - Width: {self.width}\n"
+            f"  - Order: {self.order}\n"
+            f"  - Centers: {self.centers}\n"
+        )
+        
+    
+        
+
 
 
 if __name__ == "__main__":
     grid = Grid(pixel_size=2e-6)
-    beam = Beam(grid)
-    # print(beam.field.shape)
-    # beam.gen_bessel(order=1, amplitude=1, width=10e-6)
-    beam.gen_bessel(order=2, amplitude=1, width=20e-6, centers=[0,0])
+    beam = GaussianBeam(grid)
+    beam.compute( amplitude=1, width=5e-6, centers=[50e-6,0])
+    print(beam)
 
     beam.plot()
     plt.show()
