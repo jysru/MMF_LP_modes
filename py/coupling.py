a3e7de62eb5151e718287455027453384e72282d
JSaucourt <jysru@pm.me>
2023-03-27 11:23:49 +0200

    Coupled beams seem ok now

--- py/coupling.py
+++ py/coupling.py
@@ -5,7 +5,8 @@ from grid import Grid
 from fiber import GrinFiber
 from modes import GrinLPMode
 from speckle import GrinSpeckle
-from devices import DeformableMirror
+from devices import MockDeformableMirror
+from plots import complex_image
 import beams
 
 
@@ -49,19 +50,46 @@ class GrinFiberBeamCoupler(GrinSpeckle):
         self.compose(coeffs=(self.modes_coeffs, self.orient_coeffs))
         return self.field
     
-    def plot(self, cmap: str = 'hot'):
+    def plot(self, cmap: str = 'hot', complex: bool = False, complex_hsv: bool = False):
         r = self.fiber.radius * 1e6
         extent = np.array([np.min(self.grid.x), np.max(self.grid.x), np.min(self.grid.y), np.max(self.grid.y)]) * 1e6
-        circle = plt.Circle((-self.grid.offsets[0], -self.grid.offsets[1]), r, fill=False, edgecolor='w', linestyle='--')
+        circle1 = plt.Circle((-self.grid.offsets[0], -self.grid.offsets[1]), r, fill=False, edgecolor='w', linestyle='--')
+        circle2 = plt.Circle((-self.grid.offsets[0], -self.grid.offsets[1]), r, fill=False, edgecolor='w', linestyle='--')
 
-        fig = plt.figure()
-        ax = plt.gca()
-        pl = plt.imshow(np.abs(self.speckle), cmap=cmap, extent=extent)
-        ax.add_patch(circle)
-        ax.set_xlabel("x [um]")
-        ax.set_ylabel("x [um]")
-        ax.set_title(f"GRIN fiber speckle ({self.N_modes} modes)")
-        plt.colorbar(pl, ax=ax)
+        if complex:
+            if complex_hsv:
+                fig = plt.figure()
+                ax = plt.gca()
+                pl = plt.imshow(complex_image(self.speckle), extent=extent)
+                ax.add_patch(circle1)
+                ax.set_xlabel("x [um]")
+                ax.set_ylabel("x [um]")
+                ax.set_title(f"GRIN fiber speckle ({self.N_modes} modes)")
+                return (fig, ax, pl)
+            else:
+                fig, axs = plt.subplots(1, 2, figsize=(13,4))
+                pl0 = axs[0].imshow(np.square(np.abs(self.speckle)), extent=extent, cmap=cmap)
+                pl1 = axs[1].imshow(np.angle(self.speckle), extent=extent, cmap="twilight")
+                axs[0].add_patch(circle1)
+                axs[1].add_patch(circle2)
+                axs[0].set_xlabel("x [um]")
+                axs[1].set_xlabel("x [um]")
+                axs[0].set_ylabel("y [um]")
+                axs[1].set_ylabel("y [um]")
+                axs[0].set_title(f"GRIN speckle intensity ({self.N_modes} modes)")
+                axs[1].set_title(f"GRIN speckle phase ({self.N_modes} modes)")
+                plt.colorbar(pl0, ax=axs[0])
+                plt.colorbar(pl1, ax=axs[1])
+                return (fig, axs, [pl0, pl1])
+        else:
+            fig = plt.figure()
+            ax = plt.gca()
+            pl = plt.imshow(np.square(np.abs(self.speckle)), cmap=cmap, extent=extent)
+            ax.add_patch(circle1)
+            ax.set_xlabel("x [um]")
+            ax.set_ylabel("x [um]")
+            ax.set_title(f"GRIN speckle intensity ({self.N_modes} modes)")
+            plt.colorbar(pl, ax=ax)
 
     def __str__(self) -> str:
         return (
@@ -76,25 +104,21 @@ class GrinFiberBeamCoupler(GrinSpeckle):
 
 
 if __name__ == "__main__":
-    dm = DeformableMirror()
-    dm.reduce_by(200)
+
     phase_map = 2*np.pi*np.random.rand(6,6)
+    dm = MockDeformableMirror(pixel_size=100e-6, pixel_numbers=(128,128))
     dm.apply_phase_map(phase_map)
 
-    grid = Grid(pixel_size=dm.pixel_size/2, pixel_numbers=(128,128))
+    grid = Grid(pixel_size=dm.pixel_size, pixel_numbers=dm.pixel_numbers)
     beam = beams.GaussianBeam(grid)
-    beam.compute(amplitude=1, width=20e-6, centers=[0,0])
+    beam.compute(amplitude=1, width=3500e-6, centers=[0,0])
     dm.apply_amplitude_map(beam.amplitude)
+    dm.reduce_by(200)
+    dm.plot()
 
-    newgrid = Grid(pixel_size=dm.pixel_size/2, pixel_numbers=(128,128))
-    beam = beams.GaussianBeam(newgrid)
-    beam.compute(amplitude=1, width=20e-6, centers=[0,0])
-    beam = dm.export_to_beam(beam, keep_beam_phases=False)
-    beam.plot(complex=True)
-    plt.show()
-
-    
-    coupled = GrinFiberBeamCoupler(beam=beam, N_modes=55)
+    beam.grid.reduce_by(200)
+    beam.field = dm._field_matrix
+    coupled = GrinFiberBeamCoupler(beam=beam, N_modes=25)
     coupled.plot(cmap='gray')
     print(coupled)
     plt.show()
