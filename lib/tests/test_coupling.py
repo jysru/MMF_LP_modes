4427d9acad7360848092896d791cd177aba4ba34
JSaucourt <jysru@pm.me>
2023-03-29 16:18:19 +0200

    Added test_couple_gaussian_flat_phases

--- lib/tests/test_coupling.py
+++ lib/tests/test_coupling.py
@@ -4,6 +4,7 @@ import numpy as np
 from lib.grid import Grid
 from lib.fiber import GrinFiber
 from lib.modes import GrinLPMode
+from lib.beams import GaussianBeam
 from lib.coupling import GrinFiberCoupler
 
 
@@ -34,6 +35,28 @@ class TestCouplingGrinLPMode(unittest.TestCase):
             self.assertAlmostEqual(modes_sum, 1, delta=1e-4)
         with self.subTest():
             self.assertAlmostEqual(traces_sum, 0, delta=1e-4)
+
+    def test_couple_gaussian_flat_phases(self):
+        grid = Grid(pixel_numbers=(128,128), pixel_size=0.5e-6)
+        fiber = GrinFiber(radius=26e-6)
+        i_max = 55
+
+        beam = GaussianBeam(grid)
+        beam.compute(amplitude=1, width=8000e-6, centers=[0,0])
+        beam.normalize_by_energy()
+
+        coupling = GrinFiberCoupler(beam.field, grid, fiber, N_modes=i_max)
+        coupling_ratio = np.sum(np.square(np.abs(coupling.speckle)))
+        n = fiber._neff_hnm[:coupling.N_modes, 2]
+        sum_centro = np.sum(coupling.coeffs_intensity[n == 0])
+        sum_not_centro = np.sum(coupling.coeffs_intensity[n != 0])
+
+        with self.subTest():
+            self.assertGreaterEqual(1, coupling_ratio)
+        with self.subTest():
+            self.assertAlmostEqual(coupling.total_coeffs_intensity, sum_centro)
+        with self.subTest():
+            self.assertAlmostEqual(0, sum_not_centro)
 
 
 if __name__ == "__main__":
