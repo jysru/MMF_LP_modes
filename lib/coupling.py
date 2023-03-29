5e20adc00dda69de7f4146e14da4541e081e0052
JSaucourt <jysru@pm.me>
2023-03-29 14:26:26 +0200

    Added GrinFiberCoupler and fixed imports

--- lib/coupling.py
+++ lib/coupling.py
@@ -1,13 +1,113 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-from grid import Grid
-from fiber import GrinFiber
-from modes import GrinLPMode
-from speckle import GrinSpeckle
-from devices import MockDeformableMirror
-from plots import complex_image
-import beams
+from lib.grid import Grid
+from lib.fiber import GrinFiber
+from lib.modes import GrinLPMode
+from lib.speckle import GrinSpeckle
+from lib.devices import MockDeformableMirror
+from lib.plots import complex_image
+import lib.beams as beams
+
+
+class GrinFiberCoupler(GrinSpeckle):
+
+    def __init__(self, field: np.ndarray, grid: Grid, fiber: GrinFiber = GrinFiber(), N_modes: int = 10, noise_std: float = 0) -> None:
+        super().__init__(fiber, grid, N_modes, noise_std)
+        self.field = field
+        self.coupling_matrix = None
+        self.decompose(self.N_modes)
+
+    def decompose(self, N_modes: int = 10):
+        N_modes = self.fiber._N_modes if N_modes > self.fiber._N_modes else N_modes
+        modes_coeffs = np.zeros(shape=(N_modes), dtype=np.complex64)
+        orient_coeffs = np.zeros(shape=(N_modes))
+
+        for i in range(N_modes):
+            n, m = self.fiber._neff_hnm[i, 2], self.fiber._neff_hnm[i, 3]
+            mode = GrinLPMode(n, m)
+            mode.compute(self.fiber, self.grid)
+            mode0, mode90 = mode._fields[:,:,0], mode._fields[:,:,1]
+
+            if n == 0: # Centro-symmetric mode
+                Cp = GrinSpeckle.power_overlap_integral(self.field, mode0)
+                phi = GrinSpeckle.phase_from_overlap_integral(self.field, mode0)
+                modes_coeffs[i] = np.sqrt(Cp) * np.exp(1j * phi)
+            else:
+                Cp1 = GrinSpeckle.power_overlap_integral(self.field, mode0)
+                Cp2 = GrinSpeckle.power_overlap_integral(self.field, mode90)
+                Cor = Cp1 / (Cp1 + Cp2)
+                mode_orient = np.sqrt(Cor) * mode0 +  np.sqrt(1 - Cor) * mode90
+                phi = GrinSpeckle.phase_from_overlap_integral(self.field, mode_orient)
+                modes_coeffs[i] = np.sqrt(Cp1 + Cp2) * np.exp(1j * phi)
+                orient_coeffs[i] = Cor
+
+        self.modes_coeffs = GrinSpeckle._normalize_coeffs(modes_coeffs)
+        self.orient_coeffs = orient_coeffs
+
+    def propagate(self, matrix: np.ndarray = None, complex: bool = True):
+        if matrix is None:
+            self.coupling_matrix = self.fiber.modes_coupling_matrix(complex=complex)
+        else:
+            self.coupling_matrix = matrix
+        out_coeffs = np.dot(self.coupling_matrix[:self.modes_coeffs.shape[0], :self.modes_coeffs.shape[0]], self.modes_coeffs)
+        self.compose(coeffs=(out_coeffs, self.orient_coeffs))
+        return self.field
+        
+    @property
+    def speckle(self):
+        self.compose(coeffs=(self.modes_coeffs, self.orient_coeffs))
+        return self.field
+    
+    def plot(self, cmap: str = 'hot', complex: bool = False, complex_hsv: bool = False):
+        r = self.fiber.radius * 1e6
+        extent = np.array([np.min(self.grid.x), np.max(self.grid.x), np.min(self.grid.y), np.max(self.grid.y)]) * 1e6
+        circle1 = plt.Circle((-self.grid.offsets[0], -self.grid.offsets[1]), r, fill=False, edgecolor='w', linestyle='--')
+        circle2 = plt.Circle((-self.grid.offsets[0], -self.grid.offsets[1]), r, fill=False, edgecolor='w', linestyle='--')
+
+        if complex:
+            if complex_hsv:
+                fig = plt.figure()
+                ax = plt.gca()
+                pl = plt.imshow(complex_image(self.speckle), extent=extent)
+                ax.add_patch(circle1)
+                ax.set_xlabel("x [um]")
+                ax.set_ylabel("x [um]")
+                ax.set_title(f"GRIN fiber speckle ({self.N_modes} modes)")
+                return (fig, ax, pl)
+            else:
+                fig, axs = plt.subplots(1, 2, figsize=(13,4))
+                pl0 = axs[0].imshow(np.square(np.abs(self.speckle)), extent=extent, cmap=cmap)
+                pl1 = axs[1].imshow(np.angle(self.speckle), extent=extent, cmap="twilight")
+                axs[0].add_patch(circle1)
+                axs[1].add_patch(circle2)
+                axs[0].set_xlabel("x [um]")
+                axs[1].set_xlabel("x [um]")
+                axs[0].set_ylabel("y [um]")
+                axs[1].set_ylabel("y [um]")
+                axs[0].set_title(f"GRIN speckle intensity ({self.N_modes} modes)")
+                axs[1].set_title(f"GRIN speckle phase ({self.N_modes} modes)")
+                plt.colorbar(pl0, ax=axs[0])
+                plt.colorbar(pl1, ax=axs[1])
+                return (fig, axs, [pl0, pl1])
+        else:
+            fig = plt.figure()
+            ax = plt.gca()
+            pl = plt.imshow(np.square(np.abs(self.speckle)), cmap=cmap, extent=extent)
+            ax.add_patch(circle1)
+            ax.set_xlabel("x [um]")
+            ax.set_ylabel("x [um]")
+            ax.set_title(f"GRIN speckle intensity ({self.N_modes} modes)")
+            plt.colorbar(pl, ax=ax)
+
+    def __str__(self) -> str:
+        return (
+            f"\t - Coupled power: {np.sum(np.square(np.abs(self.modes_coeffs)))}\n"
+            f"\t - Number of modes: {self.N_modes}\n"
+            f"\t - Intensity coeffs:\n"
+            f"{np.square(np.abs(self.modes_coeffs))}"
+        )
+
 
 
 class GrinFiberBeamCoupler(GrinSpeckle):
@@ -104,32 +204,48 @@ class GrinFiberBeamCoupler(GrinSpeckle):
     def __str__(self) -> str:
         return (
             f"\t - Coupled power: {np.sum(np.square(np.abs(self.modes_coeffs)))}\n"
-            f"\t - Number of modes: {self.N_modes}"
+            f"\t - Number of modes: {self.N_modes}\n"
+            f"\t - Intensity coeffs:\n"
+            f"{np.square(np.abs(self.modes_coeffs))}"
         )
     
 
 if __name__ == "__main__":
 
-    phase_map = 2*np.pi*np.random.rand(12,12)
-    dm = MockDeformableMirror(pixel_size=100e-6, pixel_numbers=(128,128))
-    dm.apply_phase_map(phase_map)
+    # phase_map = 2*np.pi*np.random.rand(12,12)
+    # dm = MockDeformableMirror(pixel_size=100e-6, pixel_numbers=(128,128))
+    # # dm.apply_phase_map(phase_map)
 
-    grid = Grid(pixel_size=dm.pixel_size, pixel_numbers=dm.pixel_numbers)
-    beam = beams.GaussianBeam(grid)
-    beam.compute(amplitude=1, width=3500e-6, centers=[0,0])
-    dm.apply_amplitude_map(beam.amplitude)
-    dm.reduce_by(200)
-    dm.plot()
+    # grid = Grid(pixel_size=dm.pixel_size, pixel_numbers=dm.pixel_numbers)
+    # beam = beams.GaussianBeam(grid)
+    # beam.compute(amplitude=1, width=3000e-6, centers=[0,0])
+    # dm.apply_amplitude_map(beam.amplitude)
+    # dm.reduce_by(200)
+    # dm.plot()
 
-    beam.grid.reduce_by(200)
-    beam.field = dm._field_matrix
-    coupled = GrinFiberBeamCoupler(beam=beam, N_modes=55)
-    print(f"Coupled energy: {np.sum(np.square(np.abs(coupled.field)))}")
-    coupled.plot(cmap='gray')
-    speck = coupled.propagate(complex=False)
-    print(f"Coupled energy: {np.sum(np.square(np.abs(speck)))}")
-    coupled.plot(cmap='gray')
-    print(coupled)
-    plt.show()
+    # beam.grid.reduce_by(200)
+    # beam.field = dm._field_matrix
+    # coupled = GrinFiberBeamCoupler(beam=beam, N_modes=55)
+    # print(f"Coupled energy: {np.sum(np.square(np.abs(coupled.field)))}")
+    # coupled.plot(cmap='gray')
+    # speck = coupled.propagate(complex=False)
+    # print(f"Coupled energy: {np.sum(np.square(np.abs(speck)))}")
+    # coupled.plot(cmap='gray')
+    # print(coupled)
+    # plt.show()
+
+
+
+    grid = Grid(pixel_numbers=(128,128), pixel_size=0.5e-6)
+    fiber = GrinFiber(radius=26e-6)
+    mode = GrinLPMode(2, 2)
+    mode.compute(fiber, grid)
+    coup = GrinFiberCoupler(mode._fields[:,:,0], grid, fiber, N_modes=55)
+    print(f"Coupled energy: {np.sum(np.square(np.abs(coup.field)))}")
+    print(coup)
+
+
+
+
 
     
