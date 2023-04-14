4371b2ba73437a0ddd76a32b1094b717c52485e2
JSaucourt <jysru@pm.me>
2023-04-14 11:32:15 +0200

    Cleaned up __str__

--- lib/fiber.py
+++ lib/fiber.py
@@ -121,17 +121,24 @@ class GrinFiber:
             return (fig, ax, pl)
     
     def __str__(self):
-        return f"""
-        {self._neff_hnm[:10]}
-        """
+        return (
+        f"{__class__.__name__} instance with:\n"
+        f"  - Radius: {self.radius * 1e6} um\n"
+        f"  - Core index: {self.n1}\n"
+        f"  - Cladding index: {self.n2}\n"
+        f"  - Wavelength: {self.wavelength * 1e9} nm\n"
+        f"  - Numerical aperture: {self._NA} um\n"
+        f"  - Number of guided LP modes: {self._N_modes}\n"
+        f"  - Number of guided LP modes (counting degenerates): {self._N_modes_degen}\n"
+        f"  - First 10 LP_n,m modes characteristics:\n"
+        f"  n_eff      h          n          m\n"
+        f"{self._neff_hnm[:10]}"
+        )
 
 
 if __name__ == "__main__":
     fiber = GrinFiber()
-    print(fiber._N_modes)
-    print(fiber._N_modes_degen)
-    # matrix = fiber.modes_coupling_matrix(complex=True)
-
-    # GrinFiber.plot_coupling_matrix(matrix, complex=True)
-    # plt.show()
+    matrix = fiber.modes_coupling_matrix(complex=True)
+    GrinFiber.plot_coupling_matrix(matrix, complex=True)
+    plt.show()
     
