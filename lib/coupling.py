67560356104cf72ee12ffb38f1a80fa80a5b24aa
JSaucourt <jysru@pm.me>
2023-04-14 17:22:07 +0200

    Added GrinFiberDegenCoupler

--- lib/coupling.py
+++ lib/coupling.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from lib.grid import Grid
 from lib.fiber import GrinFiber
 from lib.modes import GrinLPMode
-from lib.speckle import GrinSpeckle
+from lib.speckle import GrinSpeckle, DegenGrinSpeckle
 from lib.devices import MockDeformableMirror
 from lib.plots import complex_image
 import lib.beams as beams
@@ -16,6 +16,7 @@ class GrinFiberCoupler(GrinSpeckle):
         super().__init__(fiber, grid, N_modes, noise_std)
         self.field = field
         self.coupling_matrix = None
+        # self.modes_coeffs, self.orient_coeffs = self.decompose(self.N_modes)
         self.decompose(self.N_modes)
         self.recompose()
 
@@ -65,6 +66,36 @@ class GrinFiberCoupler(GrinSpeckle):
             f"\t - Intensity coeffs:\n"
             f"{np.square(np.abs(self.modes_coeffs))}"
         )
+
+
+class GrinFiberDegenCoupler(DegenGrinSpeckle):
+
+    def __init__(self, field: np.ndarray, grid: Grid, fiber: GrinFiber = GrinFiber(), N_modes: int = 10, noise_std: float = 0) -> None:
+        super().__init__(fiber, grid, N_modes, noise_std)
+        self.field = field
+        self.coupling_matrix = None
+        self.modes_coeffs = self.decompose(self.N_modes)
+        self.recompose()
+
+    def propagate(self, matrix: np.ndarray = None, complex: bool = True, full: bool = True):
+        if matrix is None:
+            self.coupling_matrix = self.fiber.modes_coupling_matrix(complex=complex, full=full, degen=True)
+        else:
+            self.coupling_matrix = matrix
+        out_coeffs = np.dot(self.coupling_matrix[:self.modes_coeffs.shape[0], :self.modes_coeffs.shape[0]], self.modes_coeffs)
+        self.compose(coeffs=(out_coeffs))
+        return self.field
+        
+    def recompose(self):
+        self.compose(coeffs=(self.modes_coeffs))
+
+    def __str__(self) -> str:
+        return (
+            f"\t - Coupled power: {np.sum(np.square(np.abs(self.modes_coeffs)))}\n"
+            f"\t - Number of modes: {self.N_modes}\n"
+            f"\t - Intensity coeffs:\n"
+            f"{np.square(np.abs(self.modes_coeffs))}"
+        )
     
 
 if __name__ == "__main__":
@@ -83,15 +114,17 @@ if __name__ == "__main__":
     beam.grid.reduce_by(200)
     beam.field = dm._field_matrix 
     beam.normalize_by_energy()
-    coupled = GrinFiberCoupler(beam.field, beam.grid, fiber=GrinFiber(), N_modes=55)
+    # coupled = GrinFiberCoupler(beam.field, beam.grid, fiber=GrinFiber(), N_modes=55)
+    # print(f"Coupled energy: {coupled.energy}")
+    coupled = GrinFiberDegenCoupler(beam.field, beam.grid, fiber=GrinFiber(), N_modes=113)
     print(f"Coupled energy: {coupled.energy}")
 
     coupled.plot(cmap='gray')
-    coupled.plot_coefficients()
+    # coupled.plot_coefficients()
     speck = coupled.propagate(complex=True)
     print(f"Speckle energy: {np.sum(np.square(np.abs(speck)))}")
     coupled.plot(cmap='gray')
-    coupled.plot_coefficients()
+    # coupled.plot_coefficients()
     coupled.fiber.plot_coupling_matrix(coupled.coupling_matrix, complex=True)
     print(np.sum(np.square(np.abs(speck))))
     print(coupled)
