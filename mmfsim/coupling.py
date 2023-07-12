94baa1fdab7a9d0c0835b8d1ff218bef1530f217
JSaucourt <jysru@pm.me>
2023-07-12 14:30:49 +0200

    Fixed couplings

--- mmfsim/coupling.py
+++ mmfsim/coupling.py
@@ -77,12 +77,63 @@ class StepIndexFiberCoupler(StepIndexSpeckle):
 
     def __init__(self, field: np.ndarray, grid: Grid, fiber: StepIndexFiber = StepIndexFiber(), N_modes: int = 10, noise_std: float = 0) -> None:
         super().__init__(fiber, grid, N_modes, noise_std)
+        self.field = field
+        self.field = self.field / self.energy
+        self.coupling_matrix = None
+        self.modes_coeffs, self.orient_coeffs = self.decompose(self.N_modes)
+        self.recompose()
+
+    def propagate(self, matrix: np.ndarray = None, complex: bool = True, full: bool = True):
+        if matrix is None:
+            self.coupling_matrix = self.fiber.modes_coupling_matrix(complex=complex, full=full)
+        else:
+            self.coupling_matrix = matrix
+        out_coeffs = np.dot(self.coupling_matrix[:self.modes_coeffs.shape[0], :self.modes_coeffs.shape[0]], self.modes_coeffs)
+        self.compose(coeffs=(out_coeffs, self.orient_coeffs))
+        return self.field
+        
+    def recompose(self):
+        self.compose(coeffs=(self.modes_coeffs, self.orient_coeffs))
+        return self.field
+
+    def __str__(self) -> str:
+        return (
+            f"\t - Coupled power: {np.sum(np.square(np.abs(self.modes_coeffs)))}\n"
+            f"\t - Number of modes: {self.N_modes}\n"
+            f"\t - Intensity coeffs:\n"
+            f"{np.square(np.abs(self.modes_coeffs))}"
+        )
 
 
 class StepIndexFiberDegenCoupler(DegenStepIndexSpeckle):
 
     def __init__(self, field: np.ndarray, grid: Grid, fiber: StepIndexFiber = StepIndexFiber(), N_modes: int = 10, noise_std: float = 0) -> None:
         super().__init__(fiber, grid, N_modes, noise_std)
+        self.field = field
+        self.field = self.field / self.energy
+        self.coupling_matrix = None
+        self.modes_coeffs = self.decompose(self.N_modes)
+        self.recompose()
+
+    def propagate(self, matrix: np.ndarray = None, complex: bool = True, full: bool = True):
+        if matrix is None:
+            self.coupling_matrix = self.fiber.modes_coupling_matrix(complex=complex, full=full, degen=True)
+        else:
+            self.coupling_matrix = matrix
+        out_coeffs = np.dot(self.coupling_matrix[:self.modes_coeffs.shape[0], :self.modes_coeffs.shape[0]], self.modes_coeffs)
+        self.compose(coeffs=(out_coeffs))
+        return self.field
+
+    def recompose(self):
+        self.compose(coeffs=(self.modes_coeffs))
+
+    def __str__(self) -> str:
+        return (
+            f"\t - Coupled power: {np.sum(np.square(np.abs(self.modes_coeffs)))}\n"
+            f"\t - Number of modes: {self.N_modes}\n"
+            f"\t - Intensity coeffs:\n"
+            f"{np.square(np.abs(self.modes_coeffs))}"
+        )
 
 
 if __name__ == "__main__":
